4f234e348a44935bab62444b5a21b9df8880feca
NaokiM03 <naokim03.lib+dev@gmail.com>
2021-10-27 18:30:23 +0900

    feat: svelte init

---
+++ snowpack.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+export default {
+  mount: {
+    public: { url: "/", static: true },
+    scrap: { url: "/scrap" },
+  },
+  plugins: [
+    "@snowpack/plugin-svelte",
+    "@snowpack/plugin-optimize",
+    "@snowpack/plugin-typescript",
+  ],
+  routes: [
+    /* Enable an SPA Fallback in development: */
+    // {"match": "routes", "src": ".*", "dest": "/index.html"},
+  ],
+  optimize: {
+    bundle: true,
+  },
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    open: "none",
+  },
+  buildOptions: {
+    baseUrl: "./",
+  },
+};
